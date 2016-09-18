5a2e662011ca8bf9a70fa3465fd15a406a80fc39
Ahmet Hamza Emra <hamza.emra@gmail.com>
2016-09-17 19:09:35 -0500

    Add files via upload

---
+++ Fixing Data Types.py
@@ -0,0 +1,27 @@
+from datetime import datetime as dt
+
+# Takes a date as a string, and returns a Python datetime object. 
+# If there is no date given, returns None
+def parse_date(date):
+    if date == '':
+        return None
+    else:
+        return dt.strptime(date, '%Y-%m-%d')
+    
+# Takes a string which is either an empty string or represents an integer,
+# and returns an int or None.
+def parse_maybe_int(i):
+    if i == '':
+        return None
+    else:
+        return int(i)
+
+# Clean up the data types in the enrollments table
+for enrollment in enrollments:
+    enrollment['cancel_date'] = parse_date(enrollment['cancel_date'])
+    enrollment['days_to_cancel'] = parse_maybe_int(enrollment['days_to_cancel'])
+    enrollment['is_canceled'] = enrollment['is_canceled'] == 'True'
+    enrollment['is_udacity'] = enrollment['is_udacity'] == 'True'
+    enrollment['join_date'] = parse_date(enrollment['join_date'])
+    
+enrollments[0]
